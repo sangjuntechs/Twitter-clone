1752255e9e3123ba63c71833bc8dc25a0cc25fa5
sangjunpark <juntech0421@github.com>
2020-09-26 19:52:33 +0900

    auth part

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+        target: { name, value}, 
+    } = event;
+    if (name === 'email') {
+        setEmail(value);
+    } else if (name ==='password') {
+        setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password)
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Github로 로그인</button>
+        <button>Google로 로그인</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
