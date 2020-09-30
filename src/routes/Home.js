41b1191859f022989665c76e06a50553e743d2a7
sangjunpark <juntech0421@github.com>
2020-09-30 21:47:16 +0900

    image upload part

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Tweet from "components/Tweet";
 const Home = ({ userObjs }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [fileAttach, setFileAttach] = useState();
 
   useEffect(() => {
     dbService.collection("tweets").onSnapshot((snapshot) => {
@@ -17,7 +18,7 @@ const Home = ({ userObjs }) => {
   }, []);
 
   const onSubmit = (event) => {
-    const toDate = new Date()
+    const toDate = new Date();
     event.preventDefault();
     dbService.collection("tweets").add({
       text: tweet,
@@ -26,7 +27,7 @@ const Home = ({ userObjs }) => {
       creatorName: userObjs.displayName,
       creatorEmail: userObjs.email,
       createDay: toDate.toLocaleDateString(),
-      createTime: toDate.toLocaleTimeString()
+      createTime: toDate.toLocaleTimeString(),
     });
     setTweet("");
   };
@@ -38,6 +39,24 @@ const Home = ({ userObjs }) => {
     setTweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setFileAttach(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearPhotoClick = () => {
+    setFileAttach(null);
+  }
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -48,7 +67,14 @@ const Home = ({ userObjs }) => {
           placeholder="당신의 생각을 적으세요."
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Tweet" />
+        {fileAttach && (
+          <div>
+            <img src={fileAttach} width="50px" height="50px" />
+            <button onClick={onClearPhotoClick}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {tweets.map((tweet) => (
