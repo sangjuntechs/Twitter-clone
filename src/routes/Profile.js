fcb3478acae4f50df6eff54f5d90cec61b5f9743
sangjunpark <juntech0421@github.com>
2020-10-01 21:58:03 +0900

    update profile nickname part

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,28 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { authService, dbService } from "fbInstance";
 import { useHistory } from "react-router-dom";
 
 export default ({ userObjs }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObjs.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObjs.displayName !== newDisplayName) {
+      await userObjs.updateProfile({
+          displayName: newDisplayName
+      });
+    }
   };
 
   const getMyTweets = async () => {
     const tweets = await dbService
       .collection("tweets")
       .where("creatorId", "==", userObjs.uid)
-      .orderBy('createAt')
+      .orderBy("createAt")
       .get();
-      console.log(tweets.docs.map((doc) => doc.data()))
+    console.log(tweets.docs.map((doc) => doc.data()));
   };
   useEffect(() => {
     getMyTweets();
-  }, );
+  },);
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="닉네임을 입력하세요."
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Nickname" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
